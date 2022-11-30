7ba654970c59925dcc5c5f1ecc2a7da969de28cb
jhy <yuns_97@naver.com>
2022-11-30 17:11:54 +0900

    BackJoon B5 10871 X보다 작은 수

--- BackJoon/Python/B5 10871 X보다 작은 수.py
+++ BackJoon/Python/B5 10871 X보다 작은 수.py
@@ -4,6 +4,6 @@ a = list(map(int, input().split()))     # 수열 A 리스트
 
 for i in range(n):
     if a[i] < x:
-        print(a[i], end=' ')             # 입력받은 순서대로 공백으로 구분 출력(end로 끝문자 지정)
+        print(a[i], end=' ')            # 입력받은 순서대로 공백으로 구분 출력(end로 끝문자 지정)
 
 # 걸린 시간 : 약 35분
