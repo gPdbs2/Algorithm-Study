135264f5e06806e9eb3fed5f4ea5926a50344bad
jhy <yuns_97@naver.com>
2023-02-03 23:31:16 +0900

    BackJoon S2 15988 1, 2, 3 더하기 3

---
+++ BackJoon/Python/S2 15988 1, 2, 3 더하기 3.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+dp = [0 for i in range(1000001)]
+dp[1], dp[2], dp[3] = 1, 2, 4
+
+for i in range(4, 1000001):
+    dp[i] = (dp[i-1]+dp[i-2]+dp[i-3])%1000000009
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
+
+# 걸린 시간 : 약 91분
